1371763d63e3095e8dae70239c2a24939e35bd3b
OrmeSile <95030000+OrmeSile@users.noreply.github.com>
2022-03-25 14:50:23 +0100

    part 1-2

--- part1/courseinfo/.history/src/App_20220325144549.js
+++ part1/courseinfo/.history/src/App_20220325144549.js
@@ -1,19 +1,18 @@
 
 const Header = (props) => (<h1>{props.course}</h1>);
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
     </div>
   )
 }
@@ -43,10 +42,7 @@ const App = () => {
         exercises2={exercises2}
         exercises3={exercises3}   
       />
-
-      <Footer
-        exercises1={exercises1} exercises2 = {exercises2} exercises3={exercises3}
-      />
+      
     </div>
   )
 }
--- part1/courseinfo/.history/src/App_20220325144847.js
+++ part1/courseinfo/.history/src/App_20220325144847.js
@@ -11,9 +11,7 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+     <Part part={props.part1} exercises={props.exercises1} />
     </div>
   )
 }
