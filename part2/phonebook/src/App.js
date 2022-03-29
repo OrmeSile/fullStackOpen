1399e2be2d6edd2f5e39502948407501ed4abaed
OrmeSile <95030000+OrmeSile@users.noreply.github.com>
2022-03-29 14:21:14 +0200

    finished part 2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,6 +4,40 @@ import Filter from './components/Filter'
 import Persons from './components/Persons'
 import personService from './services/persons'
 
+const Notification = ({ info }) => {
+  
+  const errorStyle = {
+    color: 'red',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  }
+  const successStyle = { ...errorStyle, color: 'green' }
+  
+  const status = info[0]
+  const content = info[1]
+  if (status === 0) {
+    return null;
+  }
+  if (status === -1) {
+    return (
+      <div style={errorStyle} className='error'>
+        {content}
+      </div>
+    )
+  }
+  if (status === 1) {
+    return (
+      <div style={successStyle} className="success">
+        {content}
+      </div>
+    )
+  }
+}
+
 const App = () => {
 
   useEffect(() => {
@@ -14,6 +48,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
+  const [displayMessage, setNewMessage] = useState([0,''])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -30,7 +65,16 @@ const App = () => {
       }
     } else {
       setPersons(persons.concat(personObject))
-      personService.create(personObject)
+      personService
+        .create(personObject)
+        .then(person => {
+          setNewMessage(
+            [1, `Added ${person.name}`]
+          )
+          setTimeout(() => {
+            setNewMessage([0, ''])
+          }, 3000)
+        })
     }
     setNewName('')
     setNewNumber('')
@@ -48,13 +92,24 @@ const App = () => {
     setNewSearch(event.target.value)
   }
   const handleDelete = (id) => {
-    personService.remove(id)
+    personService.remove(id).catch(e => {
+      const personName = persons.find(person => person.id === id)
+      setNewMessage(
+        [-1, `${personName.name} was already deleted`]
+      )
+      setTimeout(() => {
+        setNewMessage([0, ''])
+      }, 3000)
+    } )
     setPersons(persons.filter(p => p.id !== id))
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification
+        info={displayMessage}
+      />
       <Filter
         newSearch={newSearch}
         handleSearchFieldChange={handleSearchFieldChange}
