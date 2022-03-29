c16e16b776116d964052cbd77d804e6cbcae8a63
OrmeSile <95030000+OrmeSile@users.noreply.github.com>
2022-03-30 01:37:58 +0200

    starting 3.1-> 3.6

--- part2/course-tests/.history/src/components/Note_20220327143456.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Note = ({ note }) => {
-    return <li>{ }</li>
-}
--- part2/course-tests/.history/src/components/Note_20220327143505.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const Note = ({ note }) => {
-    return <li>{note.content }</li>
-}
-
