3218e619dde0dd2416624f7bbe08a2e3fff24e24
OrmeSile <95030000+OrmeSile@users.noreply.github.com>
2022-04-06 14:16:54 +0200

    eslint to frontend

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,32 +1,33 @@
+/* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
-const LoginForm = ({username, password, setUsername, setPassword, handleLogin}) => {
-  return (
-      <form onSubmit={handleLogin}>
-        <div>
-          <label htmlFor="Username">Username: </label>
-          <input
+const LoginForm = ({ username, password, setUsername, setPassword, handleLogin }) => {
+  return(
+    <form onSubmit={handleLogin}>
+      <div>
+        <label htmlFor="Username">Username: </label>
+        <input
           type='text'
           name='Username'
           value={username}
-          onChange={({target}) => setUsername(target.value)}
-            />
-        </div>
-        <div>
-          <label htmlFor="Password">Password: </label>
-            <input
-            type="password"
-            name='Password'
-            value={password}
-            onChange={({target}) => setPassword(target.value)}
-            />
-        </div>
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="Password">Password: </label>
+        <input
+          type="password"
+          name='Password'
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
       <button type='submit'>login</button>
-      </form>
-    )
+    </form>
+  )
 }
 
 const App = () => {
@@ -47,11 +48,13 @@ const App = () => {
     event.preventDefault()
 
     console.log('logging with', username, password)
+
     try {
       const user = await loginService.login({
         username, password
       })
       setUser(user)
+      window.localStorage.setItem('user', JSON.stringify(user))
       setUsername('')
       setPassword('')
       setSuccessMessage('Successfully logged in')
@@ -71,19 +74,20 @@ const App = () => {
       { errorMessage && <p>{ errorMessage }</p> }
       { successMessage && <p>{ successMessage }</p> }
       { user === null
-      ? <LoginForm
-        username={username}
-        password={password}
-        setUsername={setUsername}
-        setPassword={setPassword}
-        handleLogin={handleLogin}
-      />
-      : <div>
+        ? <LoginForm
+          username={username}
+          password={password}
+          setUsername={setUsername}
+          setPassword={setPassword}
+          handleLogin={handleLogin}
+        />
+        :
+        <div>
           <h2>blogs</h2>
           <div>
-          {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />
-          )}
+            {blogs.map(blog =>
+              <Blog key={blog.id} blog={blog} />
+            )}
           </div>
         </div>
       }
