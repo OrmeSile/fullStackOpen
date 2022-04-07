80db92fd5506995b5a9b1b9839cc2414d5c00faf
OrmeSile <95030000+OrmeSile@users.noreply.github.com>
2022-04-07 13:19:40 +0200

    done 5.8

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,40 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {React, useState} from 'react'
+const Blog = ({ blog }) => {
+  const [details, setDetails] = useState(false)
+    
+  const showDetails = () => {
+    setDetails(!details)
+  }
+
+  const showWhenVisible = { display: details ? '' : 'none' }
+
+  const style = {
+    border: 'solid black 2px',
+    borderRadius: '5px',
+    margin: '5px 0',
+    padding: '2px 10px'
+  }
+
+  return (
+    <div style={style}>
+      {blog.title} {blog.author}
+      <button onClick={showDetails}>
+        {details ? 'hide' : 'view'}
+      </button>
+      <div style={showWhenVisible}>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>
+          {blog.user.name}
+        </div>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
